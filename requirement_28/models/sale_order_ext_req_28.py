79286bc9766da07ac1ec938496de1bb480884166
karangitfordev <karanworks2122@gmail.com>
2024-03-12 14:01:37 +0530

    [EDIT] requirement-28

--- requirement_28/models/sale_order_ext_req_28.py
+++ requirement_28/models/sale_order_ext_req_28.py
@@ -8,41 +8,37 @@ class SaleOrder(models.Model):
         res = super(SaleOrder, self).action_confirm()
 
         for order in self:
+            lines_by_warehouse = {}
+
             for line in order.order_line:
-                warehouse_id = line.warehouse_id.id or order.warehouse_id.id
+                warehouse = line.warehouse_id or order.warehouse_id
+                if warehouse:
+                    if warehouse not in lines_by_warehouse:
+                        lines_by_warehouse[warehouse] = self.env['sale.order.line']
+                    lines_by_warehouse[warehouse] += line
 
-                # Create delivery order for each line
-                print('line - ',line)
-                picking_vals = {
-                    'origin': order.name,
-                    'partner_id': order.partner_id.id,
-                    'location_dest_id': self.env['stock.warehouse'].browse(warehouse_id).lot_stock_id.id,
-                    'location_id': line.order_id.partner_id.property_stock_customer.id,
-                    'picking_type_id':self.env['stock.warehouse'].browse(warehouse_id).pick_type_id.id
-                    # Add other necessary fields
-                }
+            for warehouse, lines in lines_by_warehouse.items():
+                if warehouse:
+                    delivery_order_vals = {
+                        'origin': order.name,
+                        'partner_id': order.partner_shipping_id.id,
+                        'location_dest_id': warehouse.lot_stock_id.id,
+                        'location_id': lines.order_id.partner_id.property_stock_customer.id,
+                        'picking_type_id':warehouse.pick_type_id.id
+                    }
 
-                print('picking_vals - ',picking_vals)
+                    delivery_order = order.env['stock.picking'].create(delivery_order_vals)
 
+                    for line in lines:
+                        move_vals = {
+                            'name': line.name,
+                            'product_id': line.product_id.id,
+                            'product_uom_qty': line.product_uom_qty,
+                            'product_uom': line.product_uom.id,
+                            'picking_id': delivery_order.id,
+                            'location_id': order.partner_id.property_stock_customer.id,
+                            'location_dest_id': warehouse.lot_stock_id.id,
 
-                picking = self.env['stock.picking'].create(picking_vals)
-
-                print('picking - ', picking)
-
-                # Add order lines to the delivery order
-                move_vals = {
-                    'name': line.name,
-                    'product_id': line.product_id.id,
-                    'product_uom_qty': line.product_uom_qty,
-                    'product_uom': line.product_uom.id,
-                    'picking_id': picking.id,
-                    'location_id':line.order_id.partner_id.property_stock_customer.id,
-                    'location_dest_id':self.env['stock.warehouse'].browse(warehouse_id).lot_stock_id.id,
-                    # Add other necessary fields
-                }
-
-                print('move_vals - ', move_vals)
-
-                self.env['stock.move'].create(move_vals)
-
+                        }
+                        order.env['stock.move'].create(move_vals)
         return res
