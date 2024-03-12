efa1456850d9163a3dc59655b20de7c51b204fd6
karangitfordev <karanworks2122@gmail.com>
2024-03-12 10:39:23 +0530

    [EDIT] requirement-28

--- requirement_28/models/sale_order_line_ext_req_28.py
+++ requirement_28/models/sale_order_line_ext_req_28.py
@@ -3,3 +3,5 @@ from odoo import fields, models
 class CustomSaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    warehouse_id = fields.Many2one('stock.warehouse',store=True,string='Warehouse',readonly=False)
+
